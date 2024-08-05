31e6551b32a994a842c086ec8e74539c0d534654
thomas fouquereau <thomas.fouquereau@gmail.com>
2024-08-05 14:17:43 +0200

    add page kanji

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 // info deroulant infini
 const content = document.getElementById('info-content');
-content.innerText = 'Bienvue sur la beta de NIHONGO (日本語) ! vous pouvez apprendre les kanji ici !';
+content.innerText = 'Bienvenu sur la beta de NIHONGO (日本語) ! vous pouvez apprendre les kanji ici !';
