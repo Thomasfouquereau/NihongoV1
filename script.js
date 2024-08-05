a1a0d45c9d9500c99a8f02d54304de1f37fbcc92
thomas fouquereau <thomas.fouquereau@gmail.com>
2024-08-05 08:01:30 +0200

    add animation info deroulant infini

---
+++ script.js
@@ -0,0 +1,3 @@
+// info deroulant infini
+const content = document.getElementById('info-content');
+content.innerText = 'Bienvue sur la beta de NIHONGO (日本語) ! vous pouvez apprendre les kanji ici !';
