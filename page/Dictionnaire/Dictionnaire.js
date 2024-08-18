bfafaa9676938930685b3a190bdd94942e6c07ac
thomas fouquereau <thomas.fouquereau@gmail.com>
2024-08-18 02:55:25 +0200

    chore: Update Dictionnaire.css to adjust menu position, add search bar functional, and improve styling

--- page/Dictionnaire/Dictionnaire.js
+++ page/Dictionnaire/Dictionnaire.js
@@ -81,3 +81,64 @@ Recherche.addEventListener('click', function () {
 fermer.addEventListener('click', function () {
     RechercheContainer.style.display = 'none';
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const searchBar = document.getElementById('rechercheInput');
+    const kanjiResults = document.getElementById('kanjiResults');
+    let kanjis = [];
+
+    // Fonction pour afficher les kanjis filtrés
+    function displayKanjis(filteredKanjis) {
+        let htmlContent = '';
+        filteredKanjis.forEach(item => {
+            htmlContent += `<div class="kanji-item">
+                <div class="kanji-meaning">
+                    <span class="kanji" translate="no">${item.Kanji}</span>
+                    <span class="meaning">${item.Meaning}</span>
+                </div>
+                <span class="on-reading" translate="no"><p>On</p>${item.OnReading.join(', ')}</span>
+                <span class="kun-reading" translate="no"><p>Kun</p>${item.KunReading.join(', ')}</span>
+                <span class="jlpt-level"><p>JLPT</p>${item.JLPTLevel}</span>`;
+            if (item.SecondaryMeaning) {
+                htmlContent += `<span class="secondary-meaning">${item.SecondaryMeaning}</span>`;
+            }
+            htmlContent += `</div>`;
+        });
+        kanjiResults.innerHTML = htmlContent;
+    }
+
+    // Fonction pour filtrer les kanjis en fonction de la recherche
+    function filterKanjis(searchTerm) {
+        if (searchTerm.length > 0) { // Afficher les résultats seulement si au moins un caractère est tapé
+            const filteredKanjis = kanjis.filter(kanji => 
+                kanji.Meaning.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            displayKanjis(filteredKanjis);
+        } else {
+            kanjiResults.innerHTML = ''; // Vider les résultats si la barre de recherche est vide
+        }
+    }
+
+    // Écouteur d'événement pour la barre de recherche
+    searchBar.addEventListener('input', function(event) {
+        const searchTerm = event.target.value;
+        filterKanjis(searchTerm);
+    });
+
+    // Charger les kanjis depuis le fichier JSON
+    fetch('../../data/kanji/liste kanji.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            kanjis = data.kanji;
+            // Ne pas afficher les kanjis au chargement initial
+        })
+        .catch(error => {
+            console.error('Erreur lors du chargement des kanjis:', error);
+        });
+});
